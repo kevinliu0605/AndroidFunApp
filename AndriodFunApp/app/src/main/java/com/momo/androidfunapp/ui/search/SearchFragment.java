00a7901c062326a7657aa5fef4c32fd4f0efe76b
kevinliu0605 <aidisheng880@gmail.com>
2022-01-21 21:14:03 -0500

    Jetpack Navigation Component

---
+++ AndriodFunApp/app/src/main/java/com/momo/androidfunapp/ui/search/SearchFragment.java
@@ -0,0 +1,31 @@
+package com.momo.androidfunapp.ui.search;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.momo.androidfunapp.R;
+
+
+public class SearchFragment extends Fragment {
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
