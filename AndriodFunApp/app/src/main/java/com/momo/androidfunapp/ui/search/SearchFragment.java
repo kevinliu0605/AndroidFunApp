fea0c7669b77070056500a64ce96796ae44ac522
kevinliu0605 <aidisheng880@gmail.com>
2022-01-24 17:36:42 -0500

    modify the search navigation, and get data from model inside of searchViewModel

--- AndriodFunApp/app/src/main/java/com/momo/androidfunapp/ui/search/SearchFragment.java
+++ AndriodFunApp/app/src/main/java/com/momo/androidfunapp/ui/search/SearchFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.momo.androidfunapp.R;
+import com.momo.androidfunapp.databinding.FragmentSearchBinding;
 import com.momo.androidfunapp.repository.NewsRepository;
 import com.momo.androidfunapp.repository.NewsViewModelFactory;
 
@@ -20,6 +22,8 @@ import com.momo.androidfunapp.repository.NewsViewModelFactory;
 public class SearchFragment extends Fragment {
 
     private SearchViewModel viewModel;
+
+    private FragmentSearchBinding binding;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -31,20 +35,35 @@ public class SearchFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        binding = FragmentSearchBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        binding.newsSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (!query.isEmpty()) {
+                    viewModel.setSearchInput(query);
+                }
+                binding.newsSearchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
         NewsRepository repository = new NewsRepository();
-        viewModel = new ViewModelProvider(this, new
-                NewsViewModelFactory(repository)).get(SearchViewModel.class);
-        viewModel.setSearchInput("Covid-19");
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository)).get(SearchViewModel.class);
+//        viewModel.setSearchInput("Covid-19");
         viewModel
                 .searchNews()
                 .observe(
